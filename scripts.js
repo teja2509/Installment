7c06ab07466ae2c94ef3db438a088f87fad7ff9f
teja2509 <84507243+teja2509@users.noreply.github.com>
2024-01-30 00:53:28 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -27,13 +27,12 @@ function markAsPaid(count) {
   const paidDateParagraph = document.createElement("p");
   paidDateParagraph.innerHTML = `<strong>Paid Date:</strong> ${currentDate}`;
 
-
   // Remove the remaining days message
   const remainingDaysMessage = submissionBox.querySelector('.remainingDaysMessage');
   if (remainingDaysMessage) {
     remainingDaysMessage.remove();
   }
-  
+
   // Check if there is an existing paid date paragraph and update it
   const existingPaidDateParagraph = submissionBox.querySelector('.paidDateParagraph');
   if (existingPaidDateParagraph) {
@@ -55,11 +54,14 @@ function markAsPaid(count) {
   paidSuccessfullyMessage.style.color = "green"; // Set the color to green or your preferred color
   submissionBox.appendChild(paidSuccessfullyMessage);
 
-  // Set a timeout to remove the message after 3 seconds
+  // Set a timeout to remove the message and reload the page after 3 seconds
   setTimeout(() => {
     paidSuccessfullyMessage.remove();
+    location.reload(); // Reload the page
   }, 3000);
 }
+
+
 
 function deleteSubmission(count) {
   // Show a confirmation dialog
@@ -82,10 +84,11 @@ function deleteSubmission(count) {
     deleteMessage.style.color = "red"; // Set the color to red or your preferred color
     submissionBox.appendChild(deleteMessage);
 
-    // Set a timeout to remove the message and the submission box after 3 seconds
+    // Set a timeout to remove the message and reload the page after 3 seconds
     setTimeout(() => {
       deleteMessage.remove();
       submissionBox.remove();
+      location.reload(); // Reload the page
     }, 3000);
   } else {
     // Display a message in the submission box for 3 seconds
@@ -199,10 +202,72 @@ function createSubmissionBox(submission, count) {
       <p><strong>Interest Month:</strong> ${submission.interestMonth} month</p>
       <div class="paidDateContainer"></div>
       <div class="buttonsContainer">
-          <button onclick="markAsPaid(${count})">Paid</button>
-          <button onclick="editSubmission(${count})">Edit</button>
-          <button onclick="deleteSubmission(${count})">Delete</button>
-          <button onclick="copySubmission(${count})">Copy</button>
+          <button class="paid" onclick="markAsPaid(${count})">Paid</button>
+          
+
+          <button class="Btn" onclick="editSubmission(${count})">Edit 
+            <svg class="svg" viewBox="0 0 512 512">
+            <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path></svg>
+          </button>
+    
+    
+          <button class="bin-button" onclick="deleteSubmission(${count})">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 39 7"
+              class="bin-top"
+            >
+              <line stroke-width="4" stroke="white" y2="5" x2="39" y1="5"></line>
+              <line
+                stroke-width="3"
+                stroke="white"
+                y2="1.5"
+                x2="26.0357"
+                y1="1.5"
+                x1="12"
+              ></line>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 33 39"
+              class="bin-bottom"
+            >
+              <mask fill="white" id="path-1-inside-1_8_19">
+                <path
+                  d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"
+                ></path>
+              </mask>
+              <path
+                mask="url(#path-1-inside-1_8_19)"
+                fill="white"
+                d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
+              ></path>
+              <path stroke-width="4" stroke="white" d="M12 6L12 29"></path>
+              <path stroke-width="4" stroke="white" d="M21 6V29"></path>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 89 80"
+              class="garbage"
+            >
+              <path
+                fill="white"
+                d="M20.5 10.5L37.5 15.5L42.5 11.5L51.5 12.5L68.75 0L72 11.5L79.5 12.5H88.5L87 22L68.75 31.5L75.5066 25L86 26L87 35.5L77.5 48L70.5 49.5L80 50L77.5 71.5L63.5 58.5L53.5 68.5L65.5 70.5L45.5 73L35.5 79.5L28 67L16 63L12 51.5L0 48L16 25L22.5 17L20.5 10.5Z"
+              ></path>
+            </svg>
+          </button>
+
+          
+          <button class="copy" onclick="copySubmission(${count})">
+          
+              <svg viewBox="0 0 512 512" class="svgIcon" height="1em"><path d="M288 448H64V224h64V160H64c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H288c35.3 0 64-28.7 64-64V384H288v64zm-64-96H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224c-35.3 0-64 28.7-64 64V288c0 35.3 28.7 64 64 64z"></path></svg>
+              <p class="text">COPY</p>
+              <span class="effect"></span>
+          </button>
+
       </div>
   `;
 
@@ -213,40 +278,44 @@ function createSubmissionBox(submission, count) {
 
 // Function to submit the form
 function submitForm(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var name = document.getElementById("name").value;
-    var dueDate = document.getElementById("dueDate").value;
-    var interestMoney = document.getElementById("interestMoney").value;
-    var interestDuration = document.getElementById("interestDuration").value;
-    var interestMonth = document.getElementById("interestMonth").value;
+  var name = document.getElementById("name").value;
+  var dueDate = document.getElementById("dueDate").value;
+  var interestMoney = document.getElementById("interestMoney").value;
+  var interestDuration = document.getElementById("interestDuration").value;
+  var interestMonth = document.getElementById("interestMonth").value;
 
-    var submittedDataContainer = document.getElementById("submittedDataContainer");
+  var submittedDataContainer = document.getElementById("submittedDataContainer");
 
-    // Create a new div for each submission
-    var newSubmissionDiv = createSubmissionBox({
-        name: name,
-        dueDate: dueDate,
-        interestMoney: interestMoney,
-        interestDuration: interestDuration,
-        interestMonth: interestMonth
-    }, ++submissionCount);
+  // Create a new div for each submission
+  var newSubmissionDiv = createSubmissionBox({
+      name: name,
+      dueDate: dueDate,
+      interestMoney: interestMoney,
+      interestDuration: interestDuration,
+      interestMonth: interestMonth
+  }, ++submissionCount);
 
-    // Save submitted data to localStorage
-    saveToLocalStorage({
-        name: name,
-        dueDate: dueDate,
-        interestMoney: interestMoney,
-        interestDuration: interestDuration,
-        interestMonth: interestMonth
-    });
+  // Save submitted data to localStorage
+  saveToLocalStorage({
+      name: name,
+      dueDate: dueDate,
+      interestMoney: interestMoney,
+      interestDuration: interestDuration,
+      interestMonth: interestMonth
+  });
 
-    // Append the new div to the container
-    submittedDataContainer.appendChild(newSubmissionDiv);
+  // Append the new div to the container
+  submittedDataContainer.appendChild(newSubmissionDiv);
 
-    // Clear the form fields after submission
-    document.getElementById("loanForm").reset();
+  // Clear the form fields after submission
+  document.getElementById("loanForm").reset();
+
+  // Reload the page
+  location.reload();
 }
+
 
 
 
@@ -267,21 +336,6 @@ function editSubmission(count) {
       editMessage.remove();
   }, 6000);
 }
-
-
-
-
-
-
-
-
-
-
-// Action when the "Copy" button is clicked
-function copySubmission(count) {
-    alert(`Copying submission #${count}.`);
-}
-
 
 
 // Function to display the paid date
@@ -305,70 +359,78 @@ loadFromLocalStorage();
 
 
 function searchData() {
-  const searchInput = document.querySelector('.search-bar input');
+  const searchInput = document.querySelector('.search input');
   const searchTerm = searchInput.value.toLowerCase();
 
   const submissions = JSON.parse(localStorage.getItem('submissions')) || [];
 
   // Filter submissions based on the search term
   const filteredSubmissions = submissions.filter(submission => {
-      const submissionValues = Object.values(submission).map(value => value.toString().toLowerCase());
-      return submissionValues.some(value => value.includes(searchTerm));
+    const submissionValues = Object.values(submission).map(value => value.toString().toLowerCase());
+    return submissionValues.some(value => value.includes(searchTerm));
   });
 
-  // Display the filtered submissions
-  const submittedDataContainer = document.getElementById("submittedDataContainer");
-  submittedDataContainer.innerHTML = '';
+  // Display the matching submissions first
+  const matchingSubmissionsContainer = document.getElementById("submittedDataContainer");
+  matchingSubmissionsContainer.innerHTML = '';
 
   filteredSubmissions.forEach((submission, index) => {
-      const newSubmissionDiv = createSubmissionBox(submission, index + 1);
-      submittedDataContainer.appendChild(newSubmissionDiv);
+    const newSubmissionDiv = createSubmissionBox(submission, index + 1);
+    matchingSubmissionsContainer.appendChild(newSubmissionDiv);
 
-      // Check if the submission is marked as paid
-      const isPaid = localStorage.getItem(`isPaid_${index + 1}`);
-      if (isPaid === "true") {
-          // Remove the remaining days message
-          const submissionBox = document.querySelector(`#submittedDataContainer .submissionBox:nth-child(${index + 1})`);
-          const remainingDaysMessage = submissionBox.querySelector('.remainingDaysMessage');
-          if (remainingDaysMessage) {
-              remainingDaysMessage.remove();
-          }
-
-          // Update the UI with the paid date
-          const paidDate = localStorage.getItem(`paidDate_${index + 1}`);
-          displayPaidDate(index + 1, paidDate);
-
-          // Hide the "Paid" and "Edit" buttons
-          const paidButton = submissionBox.querySelector('button:nth-of-type(1)'); // Assuming "Paid" is the first button
-          const editButton = submissionBox.querySelector('button:nth-of-type(2)'); // Assuming "Edit" is the second button
-          paidButton.style.display = 'none';
-          editButton.style.display = 'none';
+    // Check if the submission is marked as paid
+    const isPaid = localStorage.getItem(`isPaid_${index + 1}`);
+    if (isPaid === "true") {
+      // Remove the remaining days message
+      const submissionBox = document.querySelector(`#submittedDataContainer .submissionBox:nth-child(${index + 1})`);
+      const remainingDaysMessage = submissionBox.querySelector('.remainingDaysMessage');
+      if (remainingDaysMessage) {
+        remainingDaysMessage.remove();
       }
+
+      // Update the UI with the paid date
+      const paidDate = localStorage.getItem(`paidDate_${index + 1}`);
+      displayPaidDate(index + 1, paidDate);
+
+      // Hide the "Paid" and "Edit" buttons
+      const paidButton = submissionBox.querySelector('button:nth-of-type(1)'); // Assuming "Paid" is the first button
+      const editButton = submissionBox.querySelector('button:nth-of-type(2)'); // Assuming "Edit" is the second button
+      paidButton.style.display = 'none';
+      editButton.style.display = 'none';
+    }
+  });
+
+  // Display the remaining submissions
+  const remainingSubmissions = submissions.filter(submission => !filteredSubmissions.includes(submission));
+  const remainingSubmissionsContainer = document.getElementById("remainingSubmissionsContainer");
+
+  remainingSubmissions.forEach((submission, index) => {
+    const newSubmissionDiv = createSubmissionBox(submission, index + 1 + filteredSubmissions.length);
+    remainingSubmissionsContainer.appendChild(newSubmissionDiv);
+
+    // Check if the submission is marked as paid
+    const isPaid = localStorage.getItem(`isPaid_${index + 1 + filteredSubmissions.length}`);
+    if (isPaid === "true") {
+      // Remove the remaining days message
+      const submissionBox = document.querySelector(`#remainingSubmissionsContainer .submissionBox:nth-child(${index + 1 + filteredSubmissions.length})`);
+      const remainingDaysMessage = submissionBox.querySelector('.remainingDaysMessage');
+      if (remainingDaysMessage) {
+        remainingDaysMessage.remove();
+      }
+
+      // Update the UI with the paid date
+      const paidDate = localStorage.getItem(`paidDate_${index + 1 + filteredSubmissions.length}`);
+      displayPaidDate(index + 1 + filteredSubmissions.length, paidDate);
+
+      // Hide the "Paid" and "Edit" buttons
+      const paidButton = submissionBox.querySelector('button:nth-of-type(1)'); // Assuming "Paid" is the first button
+      const editButton = submissionBox.querySelector('button:nth-of-type(2)'); // Assuming "Edit" is the second button
+      paidButton.style.display = 'none';
+      editButton.style.display = 'none';
+    }
   });
 }
 
-
-function copySubmission(count) {
-  const submission = getSubmissionData(count);
-  
-  if (!submission) {
-      alert("Submission data not found.");
-      return;
-  }
-
-  let copyText;
-
-  if (isSubmissionPaid(count)) {
-      // After marking as paid
-      copyText = generateCopyTextPaid(submission);
-  } else {
-      // Before marking as paid
-      copyText = generateCopyTextNotPaid(submission);
-  }
-
-  copyToClipboard(copyText);
-  alert("Text copied to clipboard!");
-}
 
 function isSubmissionPaid(count) {
   return localStorage.getItem(`isPaid_${count}`) === "true";
@@ -378,8 +440,6 @@ function getSubmissionData(count) {
   const submissions = JSON.parse(localStorage.getItem('submissions')) || [];
   return submissions[count - 1];
 }
-
-
 
 function generateCopyTextNotPaid(submission) {
   const remainingDays = calculateRemainingDays(submission.dueDate);
@@ -395,18 +455,30 @@ function generateCopyTextNotPaid(submission) {
   const week2Charge = calculateInterest(submission.interestMoney, 13);
   const week3Charge = calculateInterest(submission.interestMoney, 20);
 
-  return `Hi *${submission.name}*!,
+  return `
+  Dear *${submission.name}*,
+  
+  Greetings! We hope this message finds you well.
+  
+  We'd like to remind you about your installment plan, consisting of *${submission.interestDuration} installments*. Your upcoming *${submission.interestMonth} installment* is due on *${submission.dueDate}*. As of now, you have *${remainingDays} days* left to make the payment.
+  
+  *Loan Details:*
+  *Installment Amount*: Rs. *${submission.interestMoney}*
+  
+  To ensure a smooth and hassle-free experience, we recommend making the payment on or before the due date. Failure to do so may result in additional charges.
+  
+  *Additional Charges Overview:*
 
-You have taken *${submission.interestDuration} installments*, and your *${submission.interestMonth} installment* is Rs *${submission.interestMoney}* due on *${submission.dueDate}*.
-
-You have left *${remainingDays} days*.
-
-Please pay on time to avoid *Extra Charges*.
-
-*Charges*:
-*1st* week: *8%* interest on *${submission.interestMoney}* was *${week1Charge}*
-*2nd* week: *13%* interest on *${submission.interestMoney}* was *${week2Charge}*
-From *3rd* week: *20%* interest on *${submission.interestMoney}* was *${week3Charge}*`;
+  *1st Week:* 8% interest on the principal amount of Rs. *${submission.interestMoney}* amounts to an extra charge of Rs. *${week1Charge}*.
+  
+  *2nd Week:* 13% interest on the principal amount of Rs. *${submission.interestMoney}* results in an extra charge of Rs. *${week2Charge}*.
+  
+  *From 3rd Week:* 20% interest on the principal amount of Rs. *${submission.interestMoney}* incurs an extra charge of Rs. *${week3Charge}*.
+  
+  
+  Thank you for choosing our services. Should you have any inquiries or require assistance, feel free to reach out.
+  
+  `;
 }
 
 
@@ -424,11 +496,6 @@ function displayCopyMessage(submissionBox) {
       copyMessage.remove();
   }, 3000);
 }
-
-
-
-
-
 
 
 function copySubmission(count) {
@@ -456,9 +523,20 @@ function copySubmission(count) {
 function generateCopyTextPaid(submission) {
   const currentDate = new Date().toLocaleDateString();
 
-  return `Hi *${submission.name}*!,
-
-You have successfully paid your *${submission.interestMonth} installment* of *${submission.interestMoney}* on *${currentDate}*`;
+  return `
+  Hello *${submission.name}*,
+  
+  Great news! We wanted to inform you that your payment for the *${submission.interestMonth} installment* of Rs. *${submission.interestMoney}* has been successfully received.
+  
+  *Payment Details:*
+  *Installment Amount:* Rs. *${submission.interestMoney}*
+  *Paid Date:* *${submission.paidDate || currentDate}*
+  
+  We appreciate your prompt payment, which helps keep your account in good standing. Your commitment to timely payments is instrumental in ensuring a smooth and hassle-free experience.
+  
+  Thank you for your continued trust in our services. If you have any questions or need assistance, feel free to reach out.
+  
+  `;
 }
 
 
@@ -505,3 +583,16 @@ document.addEventListener("DOMContentLoaded", updateCurrentDate);
 
 // Optionally, update the date every day (86400000 milliseconds)
 setInterval(updateCurrentDate, 86400000);  // 24 hours
+
+function clearLocalStorage() {
+  localStorage.clear();
+  alert("Local storage cleared!");
+
+
+}
+
+
+
+
+
+
